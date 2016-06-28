51a0eef78136563fd74355c09f6982c9139df986
dunglt <mr.ledung0802@gmail.com>
2016-06-28 20:25:57 +0700

    Images

--- Project/Scripts/main.js
+++ Project/Scripts/main.js
@@ -1,6 +1,22 @@
 var context;
 
 window.onload = function(){
+
+  TankOnline.game = new Phaser.Game(window.innerWidth,
+                                    window.innerHeight,
+                                    Phaser.AUTO,
+                                    '',
+                                    {preload: preload, create: create, update: update});
+
+  var preload = function(){
+    TankOnline.game.load.image('tankDown', './images/tank_player1_down_c0_t1_s1.png');
+  }
+  var create = function(){
+    var.tank = TankOnline.game.add.sprite(window.innerWidth/2, window.innerHeight/2,'tankDown');
+  }
+  var update = function(){
+    
+  }
   var canvas = document.createElement("canvas");
   document.body.appendChild(canvas);
 
